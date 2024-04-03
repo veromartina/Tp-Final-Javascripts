5ff4e4e6ff66e628128e6cbd06e6cbdfc3ff7956
veromartina <veromartina8@gmail.com>
2024-04-03 16:22:54 -0300

    se modifica el backgroundSize para la imagen

--- index.js
+++ index.js
@@ -164,7 +164,7 @@ function cargarImagen() { //función que se ejecuta cuando se presiona la tecla 
     let imagenUrl = document.getElementById("url-img-input").value; //se obtiene el valor del input
 
     imagenMeme.style.backgroundImage = "url('" + imagenUrl + "')"; //Se establece la URL de la imagen como fondo del elemento con ID "imagen-meme".
-    imagenMeme.style.backgroundSize = "cover";
+    imagenMeme.style.backgroundSize = "contain";
     document.getElementById('url-img-input').value = "";  // se borra el valor del input con ID "url-img-input" después de cargar la imagen.
 }
 
@@ -508,4 +508,3 @@ botonDescarga.addEventListener("click", () => {
         console.error("Error al convertir a imagen:", error);
       });
   });
-  
